37cbcb322ea1b47c6d8bcec8fe4e0a435afc8fc3
Thomas Liebeskind <thomas.liebeskind@audi.de>
2020-08-25 21:39:26 +0200

    updte ts

--- api/ts/test.ts
+++ api/ts/test.ts
@@ -1,5 +1,6 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
 export default (request: NowRequest, response: NowResponse) => {
+  console.log(request)
   response.status(200).send(`Hello from TS!`)
 }
